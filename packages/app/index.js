c520259c92071917f078b3f71d238a2ee2595554
Liutauras <lidi.uk@gmail.com>
2018-11-24 21:09:21 +0100

    Step 5: Add package b and use it in the app

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,5 +1,7 @@
 console.log("Main app");
 
 const a = require('a');
+const b = require('b');
 
 console.log(a());
+console.log(b());
---
+++ packages/b/index.js
@@ -0,0 +1,7 @@
+const Funnies = require('funnies').Funnies
+
+let funnies = new Funnies();
+
+module.exports = function() {
+    return 'b ' + funnies.message();
+}
